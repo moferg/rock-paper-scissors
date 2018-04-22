d2fccf8201ad11d69ff6fff9fde254252596e8d4
Marshall Ferguson <moferg01@louisville.edu>
2018-04-22 10:51:11 -0400

    Added pseudocode outline

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -10,6 +10,34 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            // Menu: Welcome to Rock Paper Scissors!
+            // Menu: Please choose either rock paper or scissors
+            // Accept user input and change string to all lowercase
+            // Generate a random number from 1-3 for cpuChoice
+                // 1 == rock
+                // 2 == paper
+                // 3 == scissors
+            
+            // if userChoice == rock && cpuChoice == rock
+                // tie game
+            // if userChoice == rock && cpuChoice == paper
+                // cpu wins
+            // if userChoice == rock && cpuChoice == scissors
+                // player wins
+            
+            // if userChoice == paper && cpuChoice == rock
+                // player wins
+            // if userChoice == paper && cpuChoice == paper
+                // tie game
+            // if userChoice == paper && cpuChocie == scissors
+                // cpu wins
+           
+            // if userChoice == scissors && cpuChoice == rock
+                // cpu wins
+            // if userChoice == scissors && cpuChoice == paper
+                // player wins
+            // if userChoice == scissors && cpuChoice == scissors
+                // tie game
         }
     }
 }
